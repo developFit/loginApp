10a3d7db67153b95a65983c295e94566e6965aba
ejmoncerrat <em@smartheads.com.ar>
2022-07-25 21:39:13 -0300

    primer commit

--- src/main/java/com/example/loginApp/controller/UserController.java
+++ src/main/java/com/example/loginApp/controller/UserController.java
@@ -1,4 +1,28 @@
 
+@RestController
 public class UserController {
-
+	
+	 
+	 @PostMapping("user")
+     public ResponseEntity<?>(String  userName , String password){
+         String token = 	  
+    	  
+      }
+	 
+	 private String getJWTToken(String userName , String Password){
+		 //validar que exista el user
+		 String secretKey = "educacionIt";
+		 
+		 String token = Jwts
+				 .builder()
+				 .setId("clase19")
+				 .setSubject(userName)
+				 .claim("role","ROLE_USER")
+				 .setIssuedAt(new Date(System.currentTimeMillis()))
+				 .setExpiration(new Date(System.currentTimeMillis()+ 600000))
+				 .signWith(SignatureAlgorithm.HS512,secretKey.getBytes()).compact();
+		 
+		 return "Bearer" + token 
+				 
+	 }
 }
